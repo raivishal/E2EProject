6c3e46d42c4c09d4cab87e710181f0db8d38988f
Vishal Rai <1995vishalrai@gmail.com>
2022-08-15 17:03:19 +0530

    updating object repository file

--- src/test/java/objects/FlightObject.java
+++ src/test/java/objects/FlightObject.java
@@ -13,6 +13,7 @@ public class FlightObject {
 	}
 	
 	By flightTab=By.xpath("//div[@class='css-1dbjc4n r-1d09ksm r-qklmqi r-1lz4bg0 r-1phboty r-18u37iz']/div[1]");
+	By oneWayTripButton=By.xpath("(//div[@class='css-1dbjc4n r-zso239'])[1]");
 	
 	
 
