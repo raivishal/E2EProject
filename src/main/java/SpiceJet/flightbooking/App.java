2c1a62c67eeddf937a2befca897a3b1e53c50385
Vishal Rai <1995vishalrai@gmail.com>
2022-08-15 16:56:20 +0530

    pushing only object repository file

---
+++ src/main/java/SpiceJet/flightbooking/App.java
@@ -0,0 +1,13 @@
+package SpiceJet.flightbooking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/objects/FlightObject.java
@@ -0,0 +1,19 @@
+package objects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class FlightObject {
+	
+	WebDriver driver;
+	
+	public FlightObject(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	
+	By flightTab=By.xpath("//div[@class='css-1dbjc4n r-1d09ksm r-qklmqi r-1lz4bg0 r-1phboty r-18u37iz']/div[1]");
+	
+	
+
+}
